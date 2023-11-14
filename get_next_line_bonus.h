6f52bbc644887640cfba46e888888203c1265196
Henri Patsi <hpatsi@c1r1p7.hive.fi>
2023-11-14 14:48:56 +0200

    some cleaning

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 14:05:52 by hpatsi            #+#    #+#             */
+/*   Updated: 2023/11/14 14:05:52 by hpatsi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+ssize_t	ft_i_strchr(const char *s, int c);
+char	*ft_strldup(const char *s1, size_t len);
+char	*ft_stradd(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+void	ft_bzero(char *s, size_t len);
+
+#endif
